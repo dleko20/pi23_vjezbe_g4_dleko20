7db474b87085a729b3550fbd32b2a69b63cc15ad
Danijela Leko <dleko20@foi.hr>
2023-05-08 16:59:20 +0200

    Dodana forma za evaluaciju

--- FrmEvaluation.cs
+++ FrmEvaluation.cs
@@ -16,5 +16,20 @@ namespace Evaluation_Manager
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
